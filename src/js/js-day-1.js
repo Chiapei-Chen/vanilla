d37e337d52fac48621953271ced8b4a0fa483228
Chiapei Chen <claire528816@gmail.com>
2024-09-27 20:46:42 +0800

    fix:update to Number

--- src/js/js-day-1.js
+++ src/js/js-day-1.js
@@ -14,18 +14,21 @@ var dayfirstsubmit = () => {
   day1formObj.job = document.getElementById("jobVal").value;
   day1formObj.company = document.getElementById("companyVal").value;
   day1formObj.seniority = document.getElementById("seniorityVal").value;
-  day1formObj.designerExp =
-    parseInt(document.getElementById("designerExp").value, 10) || 0;
-  day1formObj.developerExp =
-    parseInt(document.getElementById("developerExp").value, 10) || 0;
+  day1formObj.designerExp =document.getElementById("designerExp").value;
+  day1formObj.developerExp =document.getElementById("developerExp").value;
+  // day1formObj.designerExp =
+  //   parseInt(document.getElementById("designerExp").value, 10) || 0;
+  // day1formObj.developerExp =
+  //   parseInt(document.getElementById("developerExp").value, 10) || 0;
 
-  const totalExp = day1formObj.designerExp + day1formObj.developerExp;
+  const totalExp =Number(day1formObj.designerExp) +Number (day1formObj.developerExp);
 
   var resultText = `My name is ${day1formObj.name},I am a ${day1formObj.job} of ${day1formObj.company} for ${day1formObj.seniority} years.
 And I have more than ${totalExp} years of work experience.`;
 
   document.getElementById("result").innerHTML = resultText;
 };
+
 // document.addEventListener("DOMContentLoaded",function(){
 //   document.getElementById("submitButton").addEventListener("click", dayfirstsubmit);
 

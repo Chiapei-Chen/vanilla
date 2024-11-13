47de82b6e1c6edd6bfec53c850498f27990491e0
Chiapei Chen <claire528816@gmail.com>
2024-11-14 00:12:29 +0800

    add:Day6Asynchronous add function

--- src/js/Day6Asynchronou.js
+++ src/js/Day6Asynchronou.js
@@ -1,14 +1,39 @@
-const getUrlDomain = (url) => {
-    // 請在以下區塊作答 --->
-    const regex= /^(?:https?:\/\/)?(?:www\.)?([^\/\s]+)/;
-    
-    const domain=url.replace(regex,'$1');
+//獲取使用者查詢字
+//發送請求獲取符合關鍵字的資料
+//處理請求成功或失敗結果
+//在葉面上顯示請求結果
 
-    return domain
-  }
-  
-  const printAnswer = () => {
-    const url = document.getElementById("url_1").value
-    const resultScope = document.getElementById("result")
-    resultScope.innerHTML = getUrlDomain(url)
-  }
+let response = []
+const callApi = () => {
+  const keywords = document.getElementById("queryStr").value
+  const apiPath = `https://restcountries.com/v2/name/${keywords}?fullText=false`
+  // 請在以下作答，程式碼內容自行改寫 --->
+  axios.get(apiPath)
+  //處理請求成功
+  .then((res)=>{
+    //  儲存API返回的資料
+    response=res.data;
+    console.log(response);
+    printAnswer(keywords);
+  })
+  //處理請求失敗
+  .catch(error=>{
+    console.error(error);
+    
+  });
+   // --->
+}
+
+const printAnswer = (val) => {
+  // 請在以下作答，程式碼內容自行改寫 --->
+  const resultScope = document.getElementById("Day6AsynchronousResult")
+  //儲存結果
+  const countries = [];
+
+  response.forEach((item)=>{
+  countries.push(item.name);
+})
+  resultScope.innerHTML = `${val} 共有 ${response.length} 筆資料 <br>
+  搜尋結果： <br>${countries.join(', ')}`
+  // --->
+}

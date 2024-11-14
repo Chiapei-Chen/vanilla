aaa3f1a1ae46230a3137db1253bbfa61b9789cc2
Chiapei Chen <claire528816@gmail.com>
2024-11-14 20:17:47 +0800

    fix:Format js

--- src/js/RegexinURLTest.js
+++ src/js/RegexinURLTest.js
@@ -1,19 +1,20 @@
 const getUrlDomain = (url) => {
-    // 請在以下區塊作答 --->
-                  //移除協議部分
-   let domain=url.replace(/^https?:\/\//, '')
-                  // 移除 www. 部分
-                .replace(/^www\./, '')
-                // 移除端口號部分
-                .replace(/:[0-9]+/, '')
-                // 移除路徑部分
-                .replace(/\/.*$/, '');
+  // 請在以下區塊作答 --->
+  //移除協議部分
+  let domain = url
+    .replace(/^https?:\/\//, "")
+    // 移除 www. 部分
+    .replace(/^www\./, "")
+    // 移除端口號部分
+    .replace(/:[0-9]+/, "")
+    // 移除路徑部分
+    .replace(/\/.*$/, "");
 
-    return domain;
-  }
-  
-  const printAnswer = () => {
-    const url = document.getElementById("url_1").value
-    const resultScope = document.getElementById("RegexResult")
-    resultScope.innerHTML = getUrlDomain(url)
-  }
+  return domain;
+};
+
+const printAnswer = () => {
+  const url = document.getElementById("url_1").value;
+  const resultScope = document.getElementById("RegexResult");
+  resultScope.innerHTML = getUrlDomain(url);
+};
